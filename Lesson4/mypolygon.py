f95435099900a7fe5eebcf725282f5998164e329
audiomilan <audiomilanmusik@gmail.com>
2020-12-04 11:03:00 -0500

    Hey, I'm adding the new changes that I have made to this repository. The code now makes a square box.

--- Lesson4/mypolygon.py
+++ Lesson4/mypolygon.py
@@ -1,2 +1,15 @@
 import turtle
 bob = turtle.Turtle()
+print(bob)
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+bob.lt(90)
+turtle.mainloop()
+
+#Excercise - draw a right angle 
+
