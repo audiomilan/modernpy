a9b243906c12290150b6804bd4e1be13c273180b
audiomilan <audiomilanmusik@gmail.com>
2020-12-22 10:44:44 -0500

    I learned how to zip a list :)

---
+++ midTest/zipemup.py
@@ -0,0 +1,9 @@
+# Here is how you zip lists. First you say Zip Em Uppppppp lololol 
+
+list1 = [5, 10 , 15, 20]
+
+list2 = [25, 30, 35, 40]
+
+#Use zip function 
+# The string "abcd" is used to spread across multiple tuples in a list :)
+print(list(zip("abcd" , list1 , list2)))
