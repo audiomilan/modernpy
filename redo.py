9178015909e0f60d8cf317feb2e8e5aab1e13877
audiomilan <audiomilanmuzik.com>
2020-12-31 15:37:39 -0500

    Working with classes :)

--- redo.py
+++ redo.py
@@ -1,15 +1,17 @@
+#Working with classes 
+
 class Wrestlers:
     def __init__(self, the_rock, stone_cold, hulk_h):
         self.the_rock = the_rock
         self.stone_cold = stone_cold
         self.hulk = hulk_h
-    
+
     def Rock(self):
         return f"I will Lay the Smackdown: ({self.the_rock}) The Rock "
-    
+
     def Stone_Cold(self):
-        return f"Get my fucking beer : ({self.stone_cold}) Stone Cold"
-    
+        return f"Get my beer : ({self.stone_cold}) Stone Cold"
+
     def Hulk1(self):
         return "Im real good with math , with numbers like my dad was. Im pretty muched dialed in ({self.hulk_h}) Hulk Hogan"
 
@@ -21,7 +23,7 @@ class Old_Wrestlers:
 
     def Hardy(self):
         return f"Off the ropes with the twist of fate ({self.hardy_boys}) Hardy Boys"
-    
+
     def Undertaker1(self):
         return f"I am about to send you to the morgue ({self.undertaker}) The Undertaker"
 
